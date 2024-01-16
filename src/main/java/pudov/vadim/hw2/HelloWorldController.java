ae744336cb375316023c065c14e78b7c3081c1d8
Vadim Pudov <pvb@rambler.ru>
2024-01-16 20:48:32 +0700

    added files

---
+++ src/main/java/pudov/vadim/hw2/HelloWorldController.java
@@ -0,0 +1,15 @@
+package pudov.vadim.hw2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "Мир") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/test/java/pudov/vadim/hw2/Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pudov.vadim.hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
